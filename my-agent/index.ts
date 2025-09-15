6ebb6f7c4ce516a7803a1801f245c2d416536240
flamers <flamers@kali.macron.org>
2025-09-15 15:00:36 +0300

    Final tests

--- my-agent/index.ts
+++ my-agent/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+import { stepCountIs, streamText } from "ai";
+import { google } from "@ai-sdk/google";
+import { SYSTEM_PROMPT } from "./prompts";
+import { getFileChangesInDirectoryTool } from "./tools";
+
+const codeReviewAgent = async (prompt: string) => {
+  const result = streamText({
+    model: google("models/gemini-2.5-flash"),
+    prompt,
+    system: SYSTEM_PROMPT,
+    tools: {
+      getFileChangesInDirectoryTool: getFileChangesInDirectoryTool,
+    },
+    stopWhen: stepCountIs(10),
+  });
+
+  for await (const chunk of result.textStream) {
+    process.stdout.write(chunk);
+  }
+};
+
+// Specify which directory the code review agent should review changes in your prompt
+await codeReviewAgent(
+  "Review the code changes in '../my-agent' directory, make your reviews and suggestions file by file",
+);
